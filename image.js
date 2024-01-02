76c120dccca5f5aed34d6f57e6980cc9717bb74f
vietanh3002 <118631118+vietanh3002@users.noreply.github.com>
2024-01-02 20:21:00 +0700

    Update image.js

--- image.js
+++ image.js
@@ -1,11 +1,100 @@
 const image_default = [
   {
-    name_path: "/Users/trung/git/BackEnd/src/public/image/dish1.png",
+    name_path: "https://i.imgur.com/IORSt6s.jpg",
     dishId: 1,
   },
   {
-    name_path: "",
+    name_path: "https://i.imgur.com/m1Ft4OK.jpg",
     dishId: 2,
+
+  },
+  {
+    name_path: "https://i.imgur.com/wCp4jmV.jpg",
+    dishId: 3,
+
+  },
+  {
+    name_path: "https://i.imgur.com/4FcPWn9.jpg",
+    dishId: 4,
+
+  },
+  {
+    name_path: "https://i.imgur.com/snMQ8d8.png",
+    dishId: 5,
+
+  },
+  {
+    name_path: "https://i.imgur.com/HanqOC1.jpg",
+    dishId: 6,
+
+  },
+  {
+    name_path: "https://i.imgur.com/XzlqEOa.jpg",
+    dishId: 7,
+
+  },
+  {
+    name_path: "https://i.imgur.com/Bkp3gzb.jpg",
+    dishId: 8,
+
+  },
+  {
+    name_path: "https://i.imgur.com/JhTNZzf.jpg",
+    dishId: 9,
+
+  },
+  {
+    name_path: "https://i.imgur.com/aarezjQ.jpg",
+    dishId: 10,
+
+  },
+  {
+    name_path: "https://i.imgur.com/SFWtrqi.jpg",
+    dishId: 11,
+
+  },
+  {
+    name_path: "https://i.imgur.com/RRNjAIh.jpg",
+    dishId: 12,
+
+  },
+  {
+    name_path: "https://i.imgur.com/tnHomyK.jpg",
+    dishId: 13,
+
+  },
+  {
+    name_path: "https://i.imgur.com/nj4sGdv.jpg",
+    dishId: 14,
+
+  },
+  {
+    name_path: "https://i.imgur.com/FKGdb8o.jpg",
+    dishId: 15,
+
+  },
+  {
+    name_path: "https://i.imgur.com/EZzc2LK.jpg",
+    dishId: 16,
+
+  },
+  {
+    name_path: "https://i.imgur.com/QQbHGdN.jpg",
+    dishId: 17,
+
+  },
+  {
+    name_path: "https://i.imgur.com/euZNwkf.jpg",
+    dishId: 18,
+
+  },
+  {
+    name_path: "https://i.imgur.com/ZpH7tkd.jpg",
+    dishId: 19,
+
+  },{
+    name_path: "https://i.imgur.com/cyAvbjM.jpg",
+    dishId: 20,
 
   },
 ];
